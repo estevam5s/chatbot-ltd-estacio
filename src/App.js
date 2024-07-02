6177b808a10f3031b1868cbf85da8dabd7d4f6c8
estevam5s <estevamsouzalaureth@gmail.com>
2024-07-01 21:55:41 -0300

    :boom: melhorias no currículo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-lone-blocks */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './paginas/login';  // Corrigido: caminho relativo a partir de src
 import Cadastro from './paginas/cadastro';
 import Configuracao from './paginas/configuracao';  // Corrigido: caminho relativo a partir de src
 import Creditos from './paginas/creditos';  // Corrigido: caminho relativo a partir de src
 import Noticias from './paginas/noticias';  // Corrigido: caminho relativo a partir de src
 import Chat from './paginas/chat';  // Corrigido: caminho relativo a partir de src
-import './estilos/app.css';  // Corrigido: caminho relativo a partir de src
+import './estilos/app.css';
+{/*import Login from './paginas/login';  // Corrigido: caminho relativo a partir de src*/}  // Corrigido: caminho relativo a partir de src
 
 function App() {
   return (
@@ -21,20 +22,22 @@ function App() {
               <Link to="/noticias" className="nav-link">Notícias</Link>
             </li>
             <li className="nav-item">
-              <Link to="/configuracao" className="nav-link">Configuração</Link>
+              <Link to="/configuracao" className="nav-link">Drive</Link>
             </li>
             <li className="nav-item">
               <Link to="/creditos" className="nav-link">Créditos</Link>
             </li>
+            {/*
             <li className="nav-item">
               <Link to="/login" className="nav-link">Login</Link>
             </li>
+            */}
           </ul>
         </nav>
 
         <div className="content">
           <Routes>
-            <Route path="/login" element={<Login />} />  {/* Corrigido: caminho da rota */}
+            {/*<Route path="/login" element={<Login />} /> Corrigido: caminho da rota */}
             <Route path="/cadastro" element={<Cadastro />} />
             <Route path="/configuracao" element={<Configuracao />} />
             <Route path="/creditos" element={<Creditos />} />
